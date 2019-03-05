6d7dc4a8f7e8a67bf4be63ca4f2b2c1e8052aaaa
ludralph <raphaelumoh@gmail.com>
2019-03-05 11:51:51 +0100

    basic frontend setup

---
+++ webpack.client.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const buildDirectory = 'dist';
+const outputDirectory = `${buildDirectory}/client`;
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin([buildDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
